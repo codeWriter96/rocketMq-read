4812b079120462b94f220b3ab5c9425b7d8ba683
wym <wym@qq.com>
2023-11-29 22:10:27 +0800

    readMe-indexFile(hashMap)

--- store/src/main/java/org/apache/rocketmq/store/index/IndexFile.java
+++ store/src/main/java/org/apache/rocketmq/store/index/IndexFile.java
@@ -30,6 +30,9 @@ import org.apache.rocketmq.store.MappedFile;
 //Index是类似HashMap实现。hashSlotNum为key的数量，indexNum是单个hashSlot中链表的最大长度
 public class IndexFile {
     private static final InternalLogger log = InternalLoggerFactory.getLogger(LoggerName.STORE_LOGGER_NAME);
+
+    //单个哈希槽大小，用于存放最新的索引Key存储前文件内index的数量，也就是indexHeader的indexCount值
+    //用于找到最新的索引Key在indexFile中的位置 = 40b + 500w * 4B hashslot + hashSlotSize * 20b
     private static int hashSlotSize = 4;
     private static int indexSize = 20;
     private static int invalidIndex = 0;
@@ -101,9 +104,13 @@ public class IndexFile {
     //写入索引（存储时，key的值被转化成keyHash值存入文件中，具体key值被抛弃，因此可能查询出多条hash冲突结果）
     //https://blog.csdn.net/weixin_43767015/article/details/127561251
     public boolean putKey(final String key, final long phyOffset, final long storeTimestamp) {
+        //索引数量<总数量2000w条
         if (this.indexHeader.getIndexCount() < this.indexNum) {
+            //key的hash值
             int keyHash = indexKeyHashMethod(key);
+            //key的hash值在当前哈希槽的位置
             int slotPos = keyHash % this.hashSlotNum;
+            //key的哈希值在文件内的哈希槽位置地址，absSlotPos = 40B + slotPos * 4B
             int absSlotPos = IndexHeader.INDEX_HEADER_SIZE + slotPos * hashSlotSize;
 
             FileLock fileLock = null;
@@ -112,11 +119,15 @@ public class IndexFile {
 
                 // fileLock = this.fileChannel.lock(absSlotPos, hashSlotSize,
                 // false);
+                //获取原来该hash位置槽内的值
+                //每次有新的值到来，都会更换对应地址的值。也就是头插法，永远存新值，再由Index条目中的旧数据组成单向链表
                 int slotValue = this.mappedByteBuffer.getInt(absSlotPos);
+                //如果 原来该hash位置槽内的值小于= 0 或大于当前索引文件中存储的最大条目，则将该槽的值设置为 0
                 if (slotValue <= invalidIndex || slotValue > this.indexHeader.getIndexCount()) {
                     slotValue = invalidIndex;
                 }
 
+                //存储时间差 s
                 long timeDiff = storeTimestamp - this.indexHeader.getBeginTimestamp();
 
                 timeDiff = timeDiff / 1000;
@@ -129,6 +140,7 @@ public class IndexFile {
                     timeDiff = 0;
                 }
 
+                // 计算索引条目在文件内的位置
                 int absIndexPos =
                     IndexHeader.INDEX_HEADER_SIZE + this.hashSlotNum * hashSlotSize
                         + this.indexHeader.getIndexCount() * indexSize;
@@ -139,21 +151,28 @@ public class IndexFile {
                 this.mappedByteBuffer.putLong(absIndexPos + 4, phyOffset);
                 //4b的timeDiff
                 this.mappedByteBuffer.putInt(absIndexPos + 4 + 8, (int) timeDiff);
-                //4b的上一个发生hash冲突的索引条目的编号
+                //4b的上一个发生hash冲突的哈希槽内的值，即旧的索引数量indexCount（新值指向旧值的链表，头插法）
                 this.mappedByteBuffer.putInt(absIndexPos + 4 + 8 + 4, slotValue);
 
+                //更新哈希槽内的值为当前索引数量
                 this.mappedByteBuffer.putInt(absSlotPos, this.indexHeader.getIndexCount());
 
+                // 更新头部的起始数据
                 if (this.indexHeader.getIndexCount() <= 1) {
                     this.indexHeader.setBeginPhyOffset(phyOffset);
                     this.indexHeader.setBeginTimestamp(storeTimestamp);
                 }
 
+                // 如果旧的slotValue为0，那么表示旧的哈希槽没有使用
+                // 本次放置index时采用了一个新的哈希槽，此时hashSlotCount自增1
                 if (invalidIndex == slotValue) {
                     this.indexHeader.incHashSlotCount();
                 }
+                //索引数量++
                 this.indexHeader.incIndexCount();
+                //设置最后更新的物理位置
                 this.indexHeader.setEndPhyOffset(phyOffset);
+                //最后更新时间戳
                 this.indexHeader.setEndTimestamp(storeTimestamp);
 
                 return true;
